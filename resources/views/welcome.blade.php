004f9b9edeba4f5ade271185576c4e3d40a97a22
Agus Judistira <agus.judistira@gmail.com>
2018-02-20 15:06:50 +0100

    Tweede commit. Nog video aan het volgen.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,17 +78,20 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Agus Judistira's Blog
-                </div>
-
+                
+                    <h1>Agus Judistira's Blog</h1>
+                    <h2>Welkom!</h2>
+                @foreach ($tasks as $task)
+                    <li>{{ $task->body }}}</li>
+                @endforeach
+                <!--
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div> -->
             </div>
         </div>
     </body>

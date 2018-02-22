15d809920b9ebb1308d2440a3cbcf00f130e32fe
Agus Judistira <agus.judistira@gmail.com>
2018-02-22 17:34:15 +0100

    tabel blog toegevoegd

---
+++ database/migrations/2018_02_22_152515_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titel');
+            $table->string('subtitel');
+            $table->text('inleiding');
+            $table->text('artikel');
+            $table->timestamp('datuminvoer');
+            $table->timestamp('datumupdate');
+            $table->boolean('commentaar_toegestaan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}

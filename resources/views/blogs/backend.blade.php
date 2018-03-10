f3b08801d092279101e45af7f2e48f9d3ea8f856
Agus Judistira <agus.judistira@gmail.com>
2018-03-10 17:28:54 +0100

    opmaakverbeteringen

--- resources/views/blogs/backend.blade.php
+++ resources/views/blogs/backend.blade.php
@@ -44,7 +44,7 @@
           @foreach($blog->categories as $category)
             &lt;{{ $category->category_name }}&gt;
           @endforeach</p>
-          <p id="artikel">{!! $blog->artikel !!}</p>
+          <p id="artikel">{!! substr($blog->artikel, 0, 160) !!}...</p>
           <p>[ <a href='/edit/{{ $blog->id }}'>Wijzigen</a> ]</p>
           <hr />
       @endforeach
